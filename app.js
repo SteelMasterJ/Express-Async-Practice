18b9f8cc03bd642644d97a9f1f8cb8815dcf8c65
Joseph Steel <joseph.steel.iv@gmail.com>
2020-06-20 12:01:29 -0400

    Finished

--- app.js
+++ app.js
@@ -8,18 +8,66 @@ app.set('view engine', 'pug');
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static('public'));
 
+function asyncHandler(cb) {
+  return async (req, res, next) => {
+    try {
+      await cb(req, res, next);
+    } catch(err) {
+      res.render('error', {error: err});
+    }
+  }
+}
+
 //CALL BACKS
-function getUsers(cb){
-  fs.readFile('data.json', 'utf8', (err, data) => {
-    if (err) return cb(err);
-    const users = JSON.parse(data);
-    return cb(null, users);
+// function getUsers(cb){
+//   fs.readFile('data.json', 'utf8', (err, data) => {
+//     if (err) return cb(err);
+//     const users = JSON.parse(data);
+//     return cb(null, users);
+//   });
+// }
+
+// app.get('/', (req,res) => {
+//   getUsers((err, users) => {
+//     if(err) {
+//       res.render('error', {error: err});
+//     } else {
+//       res.render('index' , {title: "users", users: users.users})
+//     }
+//   });
+// }); 
+
+
+//promises
+function getUsers(){
+  return new Promise((resolve, reject) => {
+    fs.readFile('data.json', 'utf-8', (err, data) => {
+      if(err) {
+        reject(err);
+      } else {
+        const users = JSON.parse(data);
+        resolve(users);
+      }
+    });
   });
 }
 
-app.get('/', (req,res) => {
-  
-}); 
+// app.get('/', (req,res) => {
+//   getUsers()
+//     .then((users) => {
+//       res.render('index', {title: "users", users: users.users});
+//     })
+//     .catch((err) => {
+//       res.render('error', {error: err});
+//     })
+// }); 
 
+
+//async await example
+app.get('/', asyncHandler(async (req,res) => {
+  const users = await getUsers();
+  // throw new Error('errorrr');
+  res.render('index', {title: "users", users: users.users});
+})); 
 
 app.listen(3000, () => console.log('App listening on port 3000!'));
